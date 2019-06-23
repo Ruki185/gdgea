dcc7b0836e76651e6018070b61bba1811256c76c
MattJSP <52080646+MattJSP@users.noreply.github.com>
2019-06-23 10:30:34 +0200

    Started second level
    a second level

---
+++ Fox Color/Assets/Scripts/SendDamageCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendDamageCollider : MonoBehaviour
+{
+    public int damageValue = 1;
+    public string tag = "Player";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == tag)
+        other.gameObject.SendMessage("ApplyDamage", damageValue, SendMessageOptions.DontRequireReceiver);
+    }
+}
